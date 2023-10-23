e6958a1c1e75c6c23580306ac1bd71e5c09602d9
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-10-23 20:06:06 +0100

    Webpack setup & config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Template } = require('webpack');
+
 
 module.exports = {
-    mode:'development',
+  mode:'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
+
+  plugins : [
+    new HtmlWebpackPlugin ({
+        title :'Todo List',
+        filename: 'index.html',
+        template : './src/index.html'
+
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+
 }
