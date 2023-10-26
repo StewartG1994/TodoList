63f221681fdb3be07b4b4b54ca12c6661ab05f3f
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-10-26 17:54:53 +0100

    Todays commit

--- src/projectDom.js
+++ src/projectDom.js
@@ -1,4 +1,4 @@
-import arrowImage from './assets/images/icons/arrow.png'
+import arrowImage from './assets/images/icons/arrow.png';
 import { createProject } from './project';
 
 let projectArray = [];
@@ -16,24 +16,31 @@ function closeItem (node){
         event.target.parentNode.remove()
         document.querySelector('#projectButton').disabled = false;
 })
-
 }
 
 function addItem(item, node){
     node.addEventListener('click', event => {
+        document.querySelectorAll('.projectLi').forEach(e => e.remove());
         projectArray.push(createProject(item.value))
-        console.log(projectArray)
-        createListItem(item.value)
+        projectArray.forEach(element =>{
+            createListItem(element.projectName, projectArray.indexOf(element))
+            runProjectView()
+
+        })
         document.querySelector('#projectButton').disabled = false;
         event.target.parentNode.remove()
-
     })
 }
 
 function addProject (){
     const mainAddProjectButton = document.getElementById('addProject');
-    mainAddProjectButton.addEventListener('click', () =>{    addProjectComponent()
+    mainAddProjectButton.addEventListener('click', () =>{ 
+    addProjectComponent()
     })
+
+
+
+
 }
 
 function addProjectComponent (){
@@ -41,36 +48,53 @@ function addProjectComponent (){
     const listNode = document.querySelector('.projectList')
     document.querySelector('#projectButton').disabled = true;
 
-    const projectAddDiv = document.createElement('div')
-    projectAddDiv.classList.add('projectAddDiv')
-    const projectInput = document.createElement('input')
+    const projectAddDiv = document.createElement('div');
+    projectAddDiv.classList.add('projectAddDiv');
+    const projectInput = document.createElement('input');
     projectInput.type = 'text';
-    projectInput.placeholder = 'Add project'
+    projectInput.placeholder = 'Add project';
 
-    const addBtn = document.createElement('button')
-    addBtn.textContent = '+ '
-    classListAdd(addBtn, 'addBtn')
+    const addBtn = document.createElement('button');
+    addBtn.textContent = '+ ';
+    classListAdd(addBtn, 'addBtn');
     
-    const closeBtn = document.createElement('button')
-    closeBtn.textContent = 'X'
-    classListAdd(closeBtn, 'closeBtn')
+    const closeBtn = document.createElement('button');
+    closeBtn.textContent = 'X';
+    classListAdd(closeBtn, 'closeBtn');
 
-    projectAddDiv.append(projectInput,addBtn, closeBtn)
-    insertAfter(projectAddDiv, listNode)
-    addItem(projectInput, addBtn)
-    closeItem(closeBtn)
-    
+    projectAddDiv.append(projectInput,addBtn, closeBtn);
+    insertAfter(projectAddDiv, listNode);
+    addItem(projectInput, addBtn);
+    closeItem(closeBtn);    
 }
 
-function createListItem(projectName){  
-    const listNode = document.querySelector('.projectList')
+function createListItem(projectName, index){  
+    const listNode = document.querySelector('.projectList');
     let project = document.createElement('li');
-    project.classList.add('projectLi')
+    project.setAttribute('indexId', index)
+    project.classList.add('projectLi');
     project.textContent = projectName;
     const arrowIcon = new Image ()
-    arrowIcon.classList.add('arrowImg')
-    arrowIcon.src = arrowImage
-    project.appendChild(arrowIcon)
-    listNode.append(project)
+    arrowIcon.classList.add('arrowImg');
+    arrowIcon.src = arrowImage;
+    project.appendChild(arrowIcon);
+    listNode.append(project);
 }
+
+function projectView(){
+    const cardArea = document.querySelector('.cardArea');
+    
+
+}
+
+function runProjectView (){
+let listItem = document.querySelectorAll('.projectLi');
+let dataNumber = 0;
+listItem.forEach(item => item.addEventListener('click', (event) => {dataNumber = event.target.getAttribute('indexId')}))
+console.log(dataNumber)
+
+}
+
+
+
 export {addProject}
